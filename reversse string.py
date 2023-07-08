6a5c6ba46a6e699f548832ae20e272956975ae5f
Angana Dhar <93136582+angana-dhar@users.noreply.github.com>
2023-07-08 10:20:46 +0530

    Add files via upload

---
+++ reversse string.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# reverse_string("We will conquere COVID-19") should return "91-DIVOC ereuqnoc lliw eW"
+
+
+# In[3]:
+
+
+from collections import deque
+
+
+# In[25]:
+
+
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, val):
+        self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def is_empty(self):
+        return len(self.container) == 0
+
+    def size(self):
+        return len(self.container)
+
+def reverse_string(s):
+    stack = Stack()
+
+    for ch in s:
+        stack.push(ch)
+
+    rstr = ''
+    while stack.size()!=0:
+        rstr += stack.pop()
+
+    return rstr
+
+
+if __name__ == '__main__':
+    print(reverse_string("We will conquere COVI-19"))
+    print(reverse_string("I am the king"))
+
+
+# In[ ]:
+
+
+
+
